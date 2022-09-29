8b5e7125ad2c0549746fbcbb244fb76e042df1b2
Santhosh <santhoshshashikumar@gmail.com>
2022-09-30 02:56:10 +0530

    Static Data Complete

---
+++ app/src/main/java/com/santhosh/moviesteamingapp/view/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.santhosh.moviesteamingapp.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.santhosh.moviesteamingapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+              val intent = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
